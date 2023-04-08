bd86d0182ace1dd3d5f3535c352679c11566f0bc
PhilRug <ruggiero.pj@gmail.com>
2023-04-07 23:22:16 -0400

    starter code for note aquisition and retrieval

--- Develop/server.js
+++ Develop/server.js
@@ -16,3 +16,10 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res)) => {
+    res.json();
+};
+app.post('/api/notes', (req, res)) => {
+
+};
